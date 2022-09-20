06109bd70a123c5e036fb9638ce25e218ccc047f
oryankibandi <shreckoryan@gmail.com>
2022-09-20 14:01:36 +0300

    fix errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,22 +1,21 @@
 # Configures Nginx server using Puppet
 
 package { 'nginx':
-  ensure => installed,
+  ensure     => 'installed',
 }
 
-file_line { 'redirecting':
-    ensure => 'present',
-    path   => 'etc/nginx/sites-available/default',
-    after  => 'listen 80 defualt_server;',
-    line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
 }
 
-file { 'landing page':
-    path    => '/var/www/html/index.html':
-    content => 'Hello World!',
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
 }
 
-service {'nginx':
+service { 'nginx':
   ensure  => running,
-  require => package['nginx'],
+  require => Package['nginx'],
 }
